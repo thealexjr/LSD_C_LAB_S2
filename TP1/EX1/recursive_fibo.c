d625c70fa4dc06374dc2fc540c29ee7457d9cda7
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-10 01:09:40 +0100

    Update recursive_fibo.c

--- TP1/EX1/recursive_fibo.c
+++ TP1/EX1/recursive_fibo.c
@@ -2,5 +2,12 @@
 
 int recursive_fibo(int n)
 {
-    // write your recursive fibo code here
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    else
+    {
+        return recursive_fibo(n - 1) + recursive_fibo(n - 2);
+    }
 }
