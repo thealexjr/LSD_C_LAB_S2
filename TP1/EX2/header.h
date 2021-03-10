3a8cffd9c4f42fb2bb65402921cca097ccc3d209
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-10 01:20:48 +0100

    Update header.h

--- TP1/EX2/header.h
+++ TP1/EX2/header.h
@@ -13,7 +13,7 @@
 
 #include <stdio.h>
 #include <stdlib.h> // for rand and srand functions
-
+#include <time.h>
 void    print_table(int *tab, int size);
 void    rand_fill_table(int *tab, int size);
 int     get_third_largest(int *tab, int size);
