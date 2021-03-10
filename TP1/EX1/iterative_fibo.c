3a2b915aab33bb5248c33ca0a6f1a93ddd86d173
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-10 01:00:10 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,12 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+    int u0 = 0, u1 = 0, un = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        un = u0 + u1;
+        u0 = u1;
+        u1 = un;
+    }
+    return un;
 }
