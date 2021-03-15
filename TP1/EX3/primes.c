d72176772ee2d45010994aa9d427488f9b0bad0e
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-15 21:55:27 +0100

    Update primes.c

--- TP1/EX3/primes.c
+++ TP1/EX3/primes.c
@@ -10,7 +10,7 @@ static bool    is_prime(int n)
 {
     if (n == 2)
         return 1;
-    for (int i = 2; i < n - 1; i++)
+    for (int i = 3; i < n - 1; i++)
     {
         if (n % i == 0)
             return 0;
