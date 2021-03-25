5ffc5350b9ab9a7a73a542240f964d10d305d0cc
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-25 16:55:54 +0100

    Update array_to_list.c

--- TP2/EX_2/array_to_list.c
+++ TP2/EX_2/array_to_list.c
@@ -1,14 +1,14 @@
 #include "header.h"
 
 
-t_list array_to_list(int T[], int n)
+t_list array_to_list(int (*T)[], int n)
 {
     t_list l = NULL;
     if (n == 0)
         return l;
     for (int i = 0; i < n; i++)
     {
-        add_node(&l, T[i]);
+        add_node(&l, (*T)[i]);
     }
     return l;
 }
