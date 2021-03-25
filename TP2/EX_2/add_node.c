e7617a3a6c65efa21fc28998002d3bd55dc7bb0d
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-25 15:34:52 +0100

    Update add_node.c

--- TP2/EX_2/add_node.c
+++ TP2/EX_2/add_node.c
@@ -1,1 +1,19 @@
+#include "header."
 
+
+void add_node(t_list l, int x)
+{
+    t_list t = create_node(x);
+    if (l == NULL)
+        l = t;
+    else
+    {
+        t_list temp = l;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = t;
+    }
+    return;
+}
