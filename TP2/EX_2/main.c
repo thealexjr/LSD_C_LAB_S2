fab44510baea66d5519f79c9500a56c87d4a0bfc
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-29 22:38:43 +0200

    EX_2 TP2 DONE

--- TP2/EX_2/main.c
+++ TP2/EX_2/main.c
@@ -5,12 +5,12 @@ int main()
 {
     int T[5] = {3, 7, 9, 4, 7};
     t_list L = create_node(0);
-    add_node(L, 1);
-    add_node(L, 2);
-    add_node(L, 3);
-    add_node(L, 4);
+    add_node(&L, 1);
+    add_node(&L, 2);
+    add_node(&L, 3);
+    add_node(&L, 4);
     print_list(L);
-    t_list A = array_to_list(&T, 5);
+    t_list A = array_to_list(T, 5);
     print_list(A);
     remove_node(3, &L);
     print_list(L);
