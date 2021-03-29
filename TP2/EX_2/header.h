0016c0788240485e3cd7c97448ffbcc06d746bd8
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-03-29 22:19:13 +0200

    Update header.h

--- TP2/EX_2/header.h
+++ TP2/EX_2/header.h
@@ -15,8 +15,8 @@ typedef struct node node;
 typedef node *t_list;
 
 t_list create_node(int);
-void add_node(t_list, int);
-t_list array_to_list(int (*T)[], int n);
+void add_node(t_list *, int);
+t_list array_to_list(int T[], int n);
 void print_list(t_list);
 unsigned size(t_list);
 void remove_node(unsigned, t_list *);
