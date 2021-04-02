e84c33db63b5043d9661bed39310a3802f3133bf
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-03 01:39:48 +0200

    Create palindrome.c

---
+++ TP3/EX_1/palindrome.c
@@ -0,0 +1,15 @@
+#include <header.h>
+
+int palindrome(char *word)
+{
+    list L1 = convert(word);
+    list L2 = convertInv(word);
+    while (L1 != NULL)
+    {
+        if (L1->value != L2->value)
+            return 0;
+        L1 = L1->next;
+        L2 = L2->next;
+    }
+    return 1;
+}
