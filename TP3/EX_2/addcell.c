aee77d23661ac3ea0cdd99ea155880c13d5bc0f7
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-05-09 00:33:53 +0000

    Create addcell.c

---
+++ TP3/EX_2/addcell.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+void addcell(int x, list *L)
+{
+    list C = (list)malloc(sizeof(cell));
+    C->value = x;
+    C->next = NULL;
+    if (*L == NULL)
+        *L = C;
+    else
+    {
+        list temp = *L;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = C;
+    }
+    return;
+}
