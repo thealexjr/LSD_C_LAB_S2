98c531a38f2df2629230f63aa4ab16830a8d98fe
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-06-10 23:52:09 +0100

    Create main.c

---
+++ TP4/EX_1/main.c
@@ -0,0 +1,19 @@
+#include "header.h"
+
+int main()
+{
+    B_tree T1 = NULL;
+    B_tree T2 = NULL;
+    T1 = makeBtree(1, NULL, NULL);
+    T2 = makeBtree(5, NULL, NULL);
+    getRight(T2, &T1);
+    B_tree T3 = makeBtree(4, T1, T2);
+    B_tree T = makeBtree(6, T3, T2);
+    printf("%d", treeDepth(T));
+    printf("\n%d", diametre(T));
+    printf("\n%d", leaf_sum(T));
+    liberateTree(&T);
+    liberateTree(&T1);
+    liberateTree(&T2);
+    return 0;
+}
