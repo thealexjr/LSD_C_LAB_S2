99cb05e5930ddb605ebef0258c660dd42f465e4c
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-06-10 22:57:14 +0100

    Create RPN.c

---
+++ TP3/EX_4/RPN.c
@@ -0,0 +1,38 @@
+#include 
+
+
+int RPN(char *c){
+    t_stack **head=NULL;
+    fwrite(1,"h",1);
+    if (!isdigit(c[0])){
+            printf("Error");
+            return 0;
+        }
+  int a;
+    for (int i=0;i<strlen(c);i++){
+        if (isdigit(c[i])){
+            a=atoi(&c[i]);
+            push(head,a);
+        }
+        while (stack_len(*head)>2){
+            if (!isdigit(c[i])){
+                if (c[i]=='+')
+                        push(head,(pop(head)+pop(head)));
+                        return 0;
+                if (c[i]=='-')
+                        push(head,(pop(head)-pop(head)));
+                        return 0;
+                if (c[i]== '/')
+                        push(head,(pop(head)/pop(head)));
+                        return 0;
+                if (c[i]== '*')
+                        push(head,(pop(head)*pop(head)));
+                        return 0;   
+                if (c[i]== '%')
+                        push(head,(pop(head)%pop(head)));
+                        return 0; 
+            }
+        }
+    }
+    return pop(head);
+}
