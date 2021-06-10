151a732cc03141b4fcca034ad248adbfb4009c6b
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-06-10 23:54:11 +0100

    Update and rename liberate_tree.c to other_fncts.c

---
+++ TP4/EX_1/other_fncts.c
@@ -0,0 +1,37 @@
+#include "header.h"
+
+B_tree makeBtree(int x, B_tree T_1, B_tree T_2)
+{
+    B_tree T = (B_tree)malloc(sizeof(node));
+    T->value = x;
+    T->left = T_1;
+    T->right = T_2;
+    return T;
+}
+void getLeft(B_tree L, B_tree *T)
+{
+    if (*T == NULL)
+        exit(EXIT_FAILURE);
+    else
+        (*T)->left = L;
+}
+
+void getRight(B_tree R, B_tree *T)
+{
+    if (*T == NULL)
+        exit(EXIT_FAILURE);
+    else
+        (*T)->right = R;
+}
+void liberateTree(B_tree *T)
+{
+    B_tree temp = *T;
+    if (temp == NULL)
+        return;
+    if (temp->left != NULL)
+        liberateTree(&temp->left);
+    if (temp->right != NULL)
+        liberateTree(&temp->right);
+    free(temp);
+    *T = NULL;
+}
